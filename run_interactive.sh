829c095f9fce96ea10318d3e413c6556c87df297
shangkuns <shangkuns@nvidia.com>
2025-11-10 01:55:21 -0800

    add config pi05

---
+++ run_interactive.sh
@@ -0,0 +1,18 @@
+CACHE_ID=${SLURM_JOB_ID:-$(date +%Y%m%d-%H%M%S)}
+CACHE_DIR=/lustre/fs1/portfolios/general/users/maxzhaoshuol/all_texturecache/${CACHE_ID}
+mkdir -p "${CACHE_DIR}"
+
+srun \
+  --account general_cs_infra \
+  --partition gpu_nodes_all \
+  --time 168:00:00 \
+  --gpus 4 \
+  --pty \
+  --container-image /lustre/fs1/portfolios/general/users/maxzhaoshuol/docker/rlinf_b1k.sqsh \
+  --container-mounts "\
+${CACHE_DIR}:/root/.cache/ov:rw,\
+/lustre/fs1/portfolios/general/users/maxzhaoshuol/behavior:/lustre/fs1/portfolios/general/users/maxzhaoshuol/behavior:rw,\
+/home/maxzhaoshuol:/home/maxzhaoshuol:rw" \
+  --container-workdir /lustre/fs1/portfolios/general/users/maxzhaoshuol/behavior/RLinf \
+  --no-container-mount-home \
+  /bin/bash
